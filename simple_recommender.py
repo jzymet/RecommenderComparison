8169d49a8071bc9d24ff610ab5c9bbec852a79ec
jzymet <jzymet@gmail.com>
2020-06-26 05:17:13 -0700

    adding MAP@k for recommender evaluation

--- simple_recommender.py
+++ simple_recommender.py
@@ -1,3 +1,5 @@
+from typing import List
+from functools import lru_cache
 import pandas as pd 
 from pandas import DataFrame
 import numpy as np
@@ -5,27 +7,16 @@ from scipy.spatial import distance
 import warnings
 warnings.filterwarnings('ignore')
 
-DEFAULT_MOVIE_CSV = "ratings.csv"
-
-def load_dataframe(path: str = DEFAULT_MOVIE_CSV) -> pd.DataFrame:
-    """load pandas dataframe and return it"""
-    
-    df: DataFrame = pd.read_csv(path)
-    titles: DataFrame = pd.read_csv("movies.csv")
-    df = pd.merge(df, titles, on='movieId')
-    df.head() # print first six rows of df
-    
-    return df
-
 class RatingsData:
     """object for storing data for user ratings of items"""
 
-    def __init__(self, df: DataFrame, user_id_col: str, item_id_col: str,  title_col: str, rating_col: str, binary: bool):
+    def __init__(self, df: DataFrame, user_id_col: str, item_id_col: str,  title_col: str, rating_col: str, binary: bool = True):
         """
         :param df: dataframe
         :param user_id_col: column title for user ID
         :param item_id_col: column title for item ID
         :param rating_col: column title for ratings
+        :param binary: whether the user data are binary (e.g., purchase data) or not (e.g., ratings data)
         """
         
         self._df: DataFrame = df
@@ -44,19 +35,21 @@ class RatingsData:
 
         #matrix of user ratings of each item
         self._item_matrix: DataFrame = self._df.pivot_table(index = self._user, columns = self._item, values = self._rating)
-        print(self._item_matrix)
 
-        #splitting the user-ratings matrix into training (80%) and test (20%)
-        held_out_count = int(len(self._item_matrix)/5)
+        #splitting the user-ratings matrix into training (90%) and test (10%)
+        held_out_count = int(len(self._item_matrix)/10)
         self._held_out_matrix: DataFrame = self._item_matrix.loc[:held_out_count, :]
         self._item_matrix: DataFrame = self._item_matrix.loc[(held_out_count+1):len(self._item_matrix), :]
-        print(self._held_out_matrix)
-        print(self._item_matrix)
+        #fill missing values with average rating
+        self._item_matrix.fillna(2.5, inplace = True)
+       #for i in range(1, len(item_matrix.loc[:, 1])+1):
+              #self._item_matrix.loc[i, :] = item_matrix.loc[i, :] - item_matrix.loc[i, :].mean()
+       #print(self._item_matrix)
 
         ###if ratings data, convert held out users' ratings to 1's if rating is greater than 4, else convert to 0 
         if not binary:
             self._held_out_matrix = pd.DataFrame(np.where(self._held_out_matrix.values >= 4, 1, 0), self._held_out_matrix.index)
-        print(self._held_out_matrix) 
+            self._held_out_matrix.columns = self._item_matrix.columns 
         
         #list of titles for each item in item matrix
         self._title_list = []
@@ -66,8 +59,9 @@ class RatingsData:
         #matrix with items as rows and with mean ratings and rating count as cols
         self._ratings = DataFrame(self._df.groupby(self._item)['rating'].mean())
         self._ratings['Number_of_ratings'] = self._df.groupby(self._item)['rating'].count()
-        
-    def recommend_items(self, item_name: str, top_n: int = 5, distance_metric: str = "corr") -> None:
+
+    @lru_cache(maxsize = 1000)
+    def recommend_items(self, item_name: str, top_n: int = 5, distance_metric: str = "corr") -> pd.DataFrame:
         """
         prints top_n recommended movies having at least 100 ratings, based on given item
         :param item_name: name of item to use a seed
@@ -77,11 +71,12 @@ class RatingsData:
         if distance_metric == "corr": dist = self._correlations
         elif distance_metric == "cos": dist = self._cosine_similarities
         else: raise ValueError("The distance heuristic must be 'corr', for correlation, or 'cos', for cosine similarity.")
-    
-        print(dist(item_name, 50).sort_values(by = 'Similarity', ascending = False).head(top_n))
 
-    def _correlations(self, item_name: str, minimum_ratings: int) -> pd.Series:
-        """returns an item's correlation with all other item having at least minimum_ratings rating; drops missing values
+        recommendations = dist(item_name, 50).sort_values(by = 'Similarity', ascending = False).head(top_n)
+        return recommendations
+
+    def _correlations(self, item_name: str, minimum_ratings: int = 0) -> pd.Series:
+        """returns an item's correlation with all other items having at least minimum_ratings rating; drops missing values
         :param item_id: number to which an item is indexed
         :param minimum_ratings: minimum number of ratings an item must have
         """
@@ -89,16 +84,19 @@ class RatingsData:
 
         #vector of ratings for given item
         item_ratings = self._item_matrix[self._title_to_item[item_name]]
-
+        if (item_ratings == 2.5).all(): #check this over
+            similarity_vector = DataFrame({'Title': self._title_list, 'Ratings_count': self._ratings['Number_of_ratings']})
+            similarity_vector['Similarity'] = pd.Series([0 for x in range(len(similarity_vector.index))], index=similarity_vector.index)
+        else:
         #matrix of correlations between given item and other items, minus missing values
-        similarity_vector = DataFrame({'Similarity': self._item_matrix.corrwith(item_ratings), 'Title': self._title_list, 'Ratings_count': self._ratings['Number_of_ratings']})
-        similarity_vector.dropna(inplace = True)
+            similarity_vector = DataFrame({'Similarity': self._item_matrix.corrwith(item_ratings), 'Title': self._title_list, 'Ratings_count': self._ratings['Number_of_ratings']})
+        #similarity_vector.dropna(inplace = True)
 
         #same matrix, but subtracting all items with fewer than minimum_ratings
         similarity_vector = similarity_vector[similarity_vector['Ratings_count'] > minimum_ratings]
         
         return similarity_vector
-        
+
     def _cosine_similarities(self, item_name: str, minimum_ratings: int) -> pd.Series:
         """returns an item's correlation with all other item having at least minimum_ratings rating; drops missing values
         :param item_id: number to which an item is indexed
@@ -106,21 +104,59 @@ class RatingsData:
         """
         #but the cold start problem!
 
-        #vector of ratings for given item, with empty cells filled in with zero, and then with centering applied
-        self._item_matrix.fillna(2.5, inplace = True)
-       #for i in range(1, len(item_matrix.loc[:, 1])+1):
-       #        self._item_matrix.loc[i, :] = item_matrix.loc[i, :] - item_matrix.loc[i, :].mean()
-       #print(self._item_matrix)
-       
         item_ratings = self._item_matrix[self._title_to_item[item_name]]
-
+        if (item_ratings == 2.5).all():
+            similarity_vector = DataFrame({'Title': self._title_list, 'Ratings_count': self._ratings['Number_of_ratings']})
+            similarity_vector['Similarity'] = pd.Series([0 for x in range(len(similarity_vector.index))], index=similarity_vector.index)
+        else:    
         #matrix of cosine similarities between given item and other items
-        similarity_list = []
-        for col in self._item_matrix.columns:
-            similarity_list.append(1 - distance.cosine(item_ratings, self._item_matrix.loc[:, col]))
-        similarity_vector = DataFrame({'Similarity': similarity_list, 'Title': self._title_list, 'Ratings_count': self._ratings['Number_of_ratings']})
+            similarity_list = []
+            for col in self._item_matrix.columns:
+                similarity_list.append(1 - distance.cosine(item_ratings, self._item_matrix.loc[:, col]))
+            similarity_vector = DataFrame({'Similarity': similarity_list, 'Title': self._title_list, 'Ratings_count': self._ratings['Number_of_ratings']})
 
         #same matrix, but subtracting all items with fewer than minimum_ratings
         similarity_vector = similarity_vector[similarity_vector['Ratings_count'] > minimum_ratings]
         
         return similarity_vector
+
+    def MAP_at_k(self, k: int, dist_metr: str = "corr") -> float: 
+        """returns MAP@k metric over all user-reclist pairs* 
+        :param k: recommendation list length
+        """
+        userAPs = []
+        for _, current_user in self._held_out_matrix.iterrows():
+            APs = []
+            print("Current User: %s" % current_user)
+            for movId, val in current_user.iteritems():
+                if val == 1:
+                    print("Seed for recommendation: %s" % self._item_to_title[movId])
+                    recommendations = self.recommend_items(self._item_to_title[movId], k)
+                    print("Precision: %s" % self.average_precision(recommendations, current_user))
+                    APs.append(self.average_precision(recommendations, current_user)) #MAPs, not average precisions
+            userAPs.append(sum(APs)/k)
+        print(userAPs)
+        MAP = sum(userAPs)/len(self._held_out_matrix)
+        return MAP
+                
+    def average_precision(self, recs: DataFrame, user_Data: DataFrame) -> float:
+        """returns average precision for particular user-reclist pair
+        :param recs: dataframe of recommendations
+        :param user_Data: the Series of user ratings for the pertinent movie
+        """
+
+        user = user_Data
+        precisions = []
+        for i in range(1, len(recs)+1):
+            top_i_recs: List[ints] = recs.index.values[:i]
+            precision: float = sum(user[movId] for movId in top_i_recs)/i
+            precisions.append(precision)
+        a_prec = sum(precisions)/len(recs)
+        return a_prec
+
+def load_movie_data(ratings_data: str = "ratings.csv", movies_data: str = "movies.csv") -> RatingsData:
+
+    ratings: DataFrame = pd.read_csv(ratings_data)
+    titles: DataFrame = pd.read_csv(movies_data)
+    ratings_with_titles: DataFrame = pd.merge(ratings, titles, on='movieId')
+    return RatingsData(ratings_with_titles, "userId", "movieId", "title", "rating", False)
--- simple_recommender_test.py
+++ simple_recommender_test.py
@@ -1,10 +1,16 @@
 from simple_recommender import *
-import ml_metrics as metrics
+from sklearn.metrics import average_precision_score
 
 def test_ratings_data():
 
-    movies_data = RatingsData(load_dataframe("ratings.csv"), "userId", "movieId", "title", "rating", False)
-    movies_data.recommend_items("Toy Story (1995)", 10)
-    movies_data.recommend_items("Toy Story (1995)", 10, "cos")
+    movies_data: DataFrame = load_movie_data()
+    recommendation_count: int = 10
+    my_recs = movies_data.recommend_items("Best Men (1997)", recommendation_count)
+    user_0_hits = movies_data._held_out_matrix.iloc[0]
+    print(my_recs)
+    print(movies_data.average_precision(my_recs, user_0_hits))
+
+    print(movies_data.MAP_at_k(10))
     
-    
+    #relevant_counts: DataFrame = {}
+    #relevant_counts["Sums"] = movies_data._held_out_matrix.sum(axis = 1)
